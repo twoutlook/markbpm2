15a072f416576d9c5891000ee94c10bd96eee926
Mark <twcloudwebapp@outlook.com>
2017-03-16 15:40:50 +0800

    try PDO...

--- index.php
+++ index.php
@@ -3,16 +3,16 @@ ini_set('display_errors', 1);
 error_reporting(~0);
 echo "bpm...??????";
 
-$tns = <<<XML
-(DESCRIPTION =
-    (ADDRESS_LIST =
-      (ADDRESS = (PROTOCOL = TCP)(HOST = 10.10.0.31)(PORT = 1521))
-    )
-    (CONNECT_DATA =
-      (SERVICE_NAME = bpm)
-    )
-  )
-XML;
+// $tns = <<<XML
+// (DESCRIPTION =
+//     (ADDRESS_LIST =
+//       (ADDRESS = (PROTOCOL = TCP)(HOST = 10.10.0.31)(PORT = 1521))
+//     )
+//     (CONNECT_DATA =
+//       (SERVICE_NAME = bpm)
+//     )
+//   )
+// XML;
 
 $db_username = "bpm";
 $db_password = "bpm";
