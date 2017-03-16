4f7959276c021e2a28171f253986ec712eb5d7ef
Mark <twcloudwebapp@outlook.com>
2017-03-16 14:04:01 +0800

    test conn

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
 
 echo "bpm";
+
+<?php  
+  
+$conn = oci_connect('bpm', 'bpm', '//10.10.0.31/bpm)');  
+  
+if (!$conn) {  
+  
+    $e = oci_error();  
+  
+    print htmlentities($e['message']);  
+  
+    exit;  
+  
+}else {  
+  
+    echo "连接oracle成功！";  
+  
+}  
+  
+?> 
