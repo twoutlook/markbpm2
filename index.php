c5d7d8b9c26908892250b88d75403cd6634b8fb7
Mark <twcloudwebapp@outlook.com>
2017-03-16 15:26:53 +0800

    try PDO

--- index.php
+++ index.php
@@ -4,12 +4,18 @@ error_reporting(~0);
 echo "bpm...";
 
  
+$db = '//10.10.0.31/bpm'; //e.g. '//192.168.1.1/orcl'
+$user = 'bpm';
+$pass = 'bpm';
+
+$conn = new PDO($db,$user,$pass);
+ 
   
-$conn = oci_connect('bpm', 'bpm', '//10.10.0.31/bpm');  
+// $conn = oci_connect('bpm', 'bpm', '//10.10.0.31/bpm');  
   
 if (!$conn) {  
     echo "连接oracle  NOT 成功！";  
-    $e = oci_error();  
+    // $e = oci_error();  
     // echo $e
     // print htmlentities($e['message']);  
   
