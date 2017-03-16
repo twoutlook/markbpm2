ca87f5bd5b7cdc45a79b7bdf9f49235644e1d29f
Mark <twcloudwebapp@outlook.com>
2017-03-16 15:38:01 +0800

    multiple line string

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 ini_set('display_errors', 1);
 error_reporting(~0);
 echo "bpm...??????";
-$tns = " 
+
+$tns = <<<XML
 (DESCRIPTION =
     (ADDRESS_LIST =
       (ADDRESS = (PROTOCOL = TCP)(HOST = 10.10.0.31)(PORT = 1521))
@@ -11,12 +12,17 @@ $tns = "
       (SERVICE_NAME = bpm)
     )
   )
-       ";
+XML;
+
 $db_username = "bpm";
 $db_password = "bpm";
+
 try{
     $conn = new PDO("oci:dbname=".$tns,$db_username,$db_password);
     echo "WORKING..."
 }catch(PDOException $e){
     echo ($e->getMessage());
 }
+
+
+?>
